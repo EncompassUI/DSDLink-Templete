eb694775144f3004fb6f5afb73f1300f031b08db
Guannan <aisreamy@gmail.com>
2016-02-24 17:51:25 +0800

    tab

---
+++ res/scripts/tab.js
@@ -0,0 +1,17 @@
+ $(document).ready(function () {
+
+     (function ($) {
+         $('.nav-tabs li a').click(function (g) {
+             var tab = $(this).closest('.nav-tabs'),
+                 index = $(this).closest('li').index();
+
+             tab.find('li').removeClass('active');
+             $(this).closest('li').addClass('active');
+
+             $('.tab-content').find('.tab-pane').not('.tab-pane:eq(' + index + ')').removeClass('active');
+             $('.tab-content').find('.tab-pane:eq(' + index + ')').addClass('active');
+             g.preventDefault();
+         });
+     })(jQuery);
+
+ });
