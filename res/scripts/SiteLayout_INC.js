39d510abafdf8e448968fcf93c8f2890d4c7361d
ZhangJing <1264627088@qq.com>
2016-02-26 10:44:24 +0800

    .

--- res/scripts/SiteLayout_INC.js
+++ res/scripts/SiteLayout_INC.js
@@ -2,30 +2,22 @@
 $(document).ready(function () {
     SetLayoutHeight();
 
-    $(window).resize(function () {
+    $(window).resize(function(){
         SetLayoutHeight();
-    }).click(function (event) {
-        if (event.button !== 2) {
-            HideAllDivs(false);
-        }
     });
+  
 });
 
 function SetLayoutHeight() {
     var fullheight = $(window).height();
+    var contentheight = $('.right-panel').height();
 
-
-    var navPanel = $('.nav-panel');
-    var mainPanel = navPanel.find('.main-panel');
-    var navTab = mainPanel.find('.nav.nav-tabs');
-    var logoGroup = navPanel.find('.row:first');
     var rightPanel = $('.right-panel');
-    var supplierPanel = rightPanel;
 
-    var mainPanelHeight = fullheight - logoGroup.height();
-
-    navPanel.css('height', fullheight);
-    mainPanel.css('height', mainPanelHeight);
-    navTab.css('height', mainPanelHeight);
-    supplierPanel.css('height',fullheight);
+    if(contentheight > fullheight){
+        rightPanel.css('height',contentheight);
+    }
+    else{
+        rightPanel.css('height',fullheight);
+    }
 }
